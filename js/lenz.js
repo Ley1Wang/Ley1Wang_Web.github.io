d680de0ea5e7f4bc0b75025ea78d692a78dc0db3
Ley1 Wang <wangleyi123456abcd@163.com>
2026-04-13 14:55:57 +0800

    Update lenz.js

--- js/lenz.js
+++ js/lenz.js
@@ -10,3 +10,20 @@ function calc() {
     document.getElementById("emf").innerText = "EMF: " + emf.toFixed(2);
     document.getElementById("current").innerText = "Current: " + current.toFixed(2);
 }
+
+let pos = 0;
+let direction = 1;
+
+function animate() {
+    pos += direction * 2;
+
+    if (pos > 200 || pos < 0) {
+        direction *= -1; // 反方向
+    }
+
+    document.getElementById("magnet").style.top = pos + "px";
+
+    requestAnimationFrame(animate);
+}
+
+animate();
