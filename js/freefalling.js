767d5b4c808d31ffa7a77776920d095e00869bc1
Ley1 Wang <wangleyi123456abcd@163.com>
2026-06-02 21:43:37 +0800

    Create freefalling.js

---
+++ js/freefalling.js
@@ -0,0 +1,23 @@
+const planets = {
+    Mercury: 3.7,
+    Venus: 8.87,
+    Earth: 9.81,
+    Moon: 1.62,
+    Mars: 3.71,
+    Jupiter: 24.79,
+    Saturn: 10.44,
+    Uranus: 8.69,
+    Neptune: 11.15
+};
+
+y = 0.5 * g * t * t
+document.getElementById("time").innerText =
+t.toFixed(2);
+
+document.getElementById("velocity").innerText =
+v.toFixed(2);
+
+document.getElementById("height").innerText =
+(h0-y).toFixed(2);
+timeData.push(t);
+velocityData.push(v);
